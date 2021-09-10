323c8a2f2acd27261f3e9115f74579cad91625e1
Alec Sharp <alecsharpie@gmail.com>
2021-09-10 17:58:53 +1000

    update tests to rreflect new function

--- tests/quote_test.py
+++ tests/quote_test.py
@@ -1,7 +1,8 @@
 from moviequotes.quote import get_quote
 
 def test_type_get_quote():
-    assert type(get_quote()) == str
+    assert type(get_quote()[0]) == str and\
+        type(get_quote()[1]) == str
 
 def test_length_get_quote():
-    assert len(get_quote()) > 0
+    assert len(get_quote()[0]) > 0 and len(get_quote()[1]) > 0
